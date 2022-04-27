3c1f24800601e9951992759bde0a883fd82b53b4
Saravana Periyasamy <saravanakumar.periyasamy@gmail.com>
2022-04-26 22:34:40 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-# install ansible
 sudo apt update
+
+# install git
+sudo apt install git
+
+# install ansible
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 
 # invoke ansible
-ansible-pull -U https://github.com/sara4dev/ubuntu.git
+sudo ansible-pull -U https://github.com/sara4dev/ubuntu.git
